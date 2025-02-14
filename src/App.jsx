86f36e9dfef8d9bf8b3c8d8a591a55393d588ff0
justin <justinlvell18@gmail.com>
2025-02-14 16:37:28 +0700

    fixing home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./home";
+import NewHome from "./NewHome";
 import Gaz from "./Gaz";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<NewHome />} />
         <Route path="/Gaz" element={<Gaz/>}/>
       </Routes>
     </Router>
